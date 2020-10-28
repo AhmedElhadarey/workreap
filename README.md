efd7d2370603711f3aab847b7250881ca873c38a
Sanjit <sanjit@sanjitchakrabarti.com>
2020-10-28 09:03:53 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# workreap-react-files-dm-website
+
+
+Android and IOS react app for digitalmarket.com
+
+change version in android/app/build.gradle -> 'defaultConfig'
+
+react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
+
+
+and then run cd android && ./gradlew assembleRelease
+
+
+ 
---
+++ src/Constants/Constant.js
@@ -0,0 +1,186 @@
+export const BaseUrl = "https://digitalmarket.com/wp-json/api/v1/";
+export const color = "#ffffff";
+export const primaryColor = "#9933CC80";
+export const statusBarColor =  "#98c73a";
+
+//For CustomHeader
+export const searchHeader = "I'm Looking for";
+
+//For Home Screen
+export const HomeCategories = "Explore Categories";
+export const HomeCategoriesTagLine = "Search by Categories";
+export const HomeFreelancer = "Featured Gigster"; 
+export const HomeFreelancerTagLine = "People you can rely on";
+export const HomeJobs = "Latest posted Guru Requirements";
+export const HomeJobsTagLine = "Start Today for Better";
+export const HomeServices = "Featured Gigs";
+export const HomeServiceTagLine = "Start Today for Better";
+
+//For Welcome Screen
+export const welcomeMain = "Let's get to outsourcing!";
+export const welcomeSkip = "Click here to continue";
+
+//For Drawer Screen
+export const DrawerGuest = "Guest";
+export const DrawerGreeting = "Greeting";
+export const DrawerLogin = "Login";
+export const DrawerAboutus = "About Us";
+export const DrawerRateApp = "Rate this App";
+export const DrawerInviteFriends = "Invite Friends";
+export const DrawerContact = "Contact Support";
+export const DrawerFavorite = "Favorite";
+export const DrawerLogout = "Logout";
+export const DrawerDashboard = "Dashboard";
+export const DrawerGeneral = "General";
+export const DrawerInbox = "Inbox";
+export const DrawerInsight = "Insight";
+export const DrawerPackages = "Packages";
+export const DrawerManageServices = "Manage Gigs";
+export const DrawerPostService = "Post a Gig";
+export const DrawerPostedServices = "Posted Gigs";
+export const DrawerAddonServices = "Addons Gigs";
+export const DrawerOngoingServices = "Ongoing Gigs";
+export const DrawerCompletedServices = "Completed Gigs";
+export const DrawerCancelledServices = "Cancelled Gigs";
+export const DrawerManageJobs = "Manage Guru Requirements";
+export const DrawerPostJob = "Post a Guru Requirement";
+export const DrawerPostedJobs = "Posted Guru Requirements";
+export const DrawerProposalJobs = "Your Proposals";
+export const DrawerOngoingJobs = "Ongoing Guru Requirements";
+export const DrawerCompletedJobs = "Completed Guru Requirements";
+export const DrawerCancelledJobs = "Cancelled Guru Requirements";
+
+//For Login Screen
+export const LoginHeader = "Sign In";
+export const Loginmain = "Login now and checkout your favourite gigs!";
+export const LoginEmail = "Email";
+export const LoginPassword = "Password";
+export const LoginButton = "SIGN IN";
+export const LoginForget = "Forget Password";
+export const LoginMoveSignup = "Don't have Account? Sign Up";
+
+//For Signup Screen
+export const SignupHeader = "Register Now";
+export const Signupmain = "Welcome to the world of outsourcing!";
+export const SignupPersonal = "Personal Details:";
+export const SignupMale = "Male";
+export const SignupFemale = "Female";
+export const SignupFname = "First Name";
+export const SignupLname = "Last Name";
+export const SignupUname = "User Name";
+export const SignupEmail = "Email";
+export const SignupPassword = "Password";
+export const SignupRetypePassword = "Retype Password";
+export const SignupLocation = "Your Location:";
+export const SignupStartas = "Start as:";
+export const SignupCompany = "Company";
+export const SignupFreelancer = "Gigster";
+export const SignupNoEmp = "No. of Employees";
+export const SignupDepartment = "Department";
+export const SignupContinue = "CONTINUE";
+export const SignupMoveSignin = "Already have account? Sign In";
+
+//For Forget Screen
+export const ForgetHeader = "Reset Password";
+export const Forgetmain = "Provide Email to reset password";
+export const ForgetButton = "Reset Password";
+export const ForgetMoveSignup = "Don't have Account? Sign Up";
+
+//For verifyAccount Screen
+export const VerifyAccountHeader = "Verify Account";
+export const VerifyAccountmain = "Provide your email please!";
+export const VerifyAccountCode = "Code";
+export const VerifyAccountButton = "Verify Account";
+
+//For ProfileSetting Screen 
+export const ProfileSettignBanner = "Change Banner";
+export const ProfileSettignDetail = "Your Details";
+export const ProfileSettignGender = "Select Gender";
+export const ProfileSettignFname = "First Name";
+export const ProfileSettignLname = "last Name";
+export const ProfileSettignRate = "Your Hourly Rate($)";
+export const ProfileSettignLogin = "Login";
+export const ProfileSettignTagline = "Your Tagline";
+export const ProfileSettignLocation = "Your Locaton";
+export const ProfileSettignCountry = "Select Country";
+export const ProfileSettignAddress = "Your Address";
+export const ProfileSettignLatitude = "Enter Latitude";
+export const ProfileSettignLogitude = "Enter Logitude";
+export const ProfileSettignCompanyDetails = "Company Details";
+export const ProfileSettignNoEmp = "No. of Emplouees you have";
+export const ProfileSettignSave = "Save Settings";
+
+//For Search Screen 
+export const SearchHadder = "Start Your Search";
+export const SearchNarrowMain = "Narrow Your Search";
+export const SearchNarrow = "Narrow Your Search";
+export const SearchFreelancer = "Gigster";
+export const SearchJobs = "Guru Requirements";
+export const SearchEmployer = "Guru";
+export const SearchServices = "Gigs";
+export const SearchType = "Type Keyword Here";
+export const SearchFreelancerType = "Gigster Type";
+export const SearchEnglishLevel = "English Level";
+export const SearchSkillRequired = "Skill Required";
+export const Searchlanguages = "Languages";
+export const SearchLocation = "Your Location";
+export const SearchLength = "Project Length";
+export const SearchCategories = "Guru Requirement Categories";
+export const SearchPriceRange = "Price Range";
+export const SearchNoEmp = "No. of Employees";
+export const SearchDelivery = "Delivery Time";
+export const Searchresponse = "Response Time";
+export const SearchButtonFreelancer = "Search Gigster";
+export const SearchButtonJobs = "Search Guru Requirements";
+export const SearchButtonEmployers = "Search Guru";
+export const SearchButtonService = "Search Gigs";
+
+//For DetailJob Screen 
+export const DetailjobHadder = "Detail Guru Requirements";
+export const DetailjobCarrer = "Career Level";
+export const Detailjoblocation = "Location";
+export const DetailjobJobType = "Guru Requirement Type";
+export const DetailjobDuration = "Duration";
+export const DetailjobAmount = "Amount";
+export const DetailjobProjectDetail = "Project Details";
+export const DetailjobSkill = "Skilled Required";
+export const DetailjobAttachment = "Attachments";
+export const DetailjobProposal = "Send Proposal";
+
+//For Detailservice Screen
+export const DetailServiceHadder = "Detail Gig";
+export const DetailServiceDelivery = "Delivery Time";
+export const DetailServiceViews = "Views";
+export const DetailServiceSales = "Sales";
+export const DetailServiceResponse = "Response Time";
+export const DetailServiceFeedback = "Feedback";
+export const DetailServiceServices = "Gigs in Queue";
+export const DetailServiceClick = "Click to Save";
+export const DetailServiceMain = "*This price is nor accurate as mentioned aboveit vary as per \n work nature";
+export const DetailServiceButtonSave = "Buy Now";
+export const DetailServiceDetail = "Gig Detail";
+export const DetailServicStarting = "starting from";
+
+//For Detailfreelancer Screen
+export const DetailFreelancerHourly = "Hourly Rate";
+export const DetailFreelancerLocation = "Location";
+export const DetailFreelancerFeedback = "Feedback";
+export const DetailFreelancerMember = "Member Since";
+export const DetailFreelancerAbout = "About";
+export const DetailFreelancerStatus = "Project Status";
+export const DetailFreelancerOngoing = "Ongoing \n Projects";
+export const DetailFreelancerCompleted = "Completed \n Projects";
+export const DetailFreelancerCancelled = "Cancelled \n Projects";
+export const DetailFreelancerServed = "Served \n Projects";
+export const DetailFreelancerSave = "Send Offer";
+export const DetailFreelancerClient = "Client Feedback";
+export const DetailFreelancerSkills= "My Skills";
+export const DetailFreelancerAward = "Award & Certifications";
+export const DetailFreelancerCrafted = "Crafted Projects";
+export const DetailFreelancerExperience= "Experience";
+export const DetailFreelancerEducation = "Education";
+
+export const InsightHeaderTitle = "Dashboard";
+
+export const OopsText="Oops,";
+export const NoDataText="No Data Available";
